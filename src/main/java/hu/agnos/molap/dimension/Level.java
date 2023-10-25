5d54e46566c09215ebc33ebf06c1224cfbb4540e
parisek <parisek@gmail.com>
2023-10-25 17:11:55 +0200

    id, code and name attributes come back to Level

--- src/main/java/hu/agnos/molap/dimension/Level.java
+++ src/main/java/hu/agnos/molap/dimension/Level.java
@@ -32,4 +32,32 @@ public class Level implements java.io.Serializable {
      */
     private final String name;
 
+    /**
+     * A szinthez tartozó azonosítót tartalamzó oszlop neve, szerepe a
+     * cube-ról készült reportnál jelentkezik
+     */
+    private final String idColumnName;
+    
+    /**
+     * A szinthez tartozó kódot tartalamzó oszlop neve, szerepe a
+     * cube-ról készült reportnál jelentkezik
+     */
+    private final String codeColumnName;
+    
+    /**
+     * A szinthez tartozó nevet tartalamzó oszlop neve, szerepe a
+     * cube-ról készült reportnál jelentkezik
+     */    
+    private final String nameColumnName;
+
+    public Level(int depth, String name) {
+        this.depth = depth;
+        this.name = name;
+        idColumnName=null;
+        codeColumnName=null;
+        nameColumnName=null;
+    }
+
+    
+    
 }
